fc2124cc14ca32c14a6bbe265abacf27e4960764
Ruel Tingling <rvtingling@gmail.com>
2019-10-26 10:02:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(numbers)
+    numbers.sort
+end 
+
+  
+def sort_array_desc(numbers)
+ numbers.sort.reverse 
+end 
+
+
+def sort_array_char_count(string)
+  new_sting = string.sort {|left, right|
+      left.length <=> right.length}
+end 
+
+def swap_elements(array)
+  new_array = array[0..-3]
+  new_array << array[-1]
+  new_array << array[-2]
+  new_array
+end 
+  
+def reverse_array(array)
+  copy = array.reverse
+end 
+  
+def kesha_maker(string_array)
+  string_array = ["bl$ke","as$ley","sc$tt"]
+end 
+
+def find_a(string)
+  #start_with? "a"
+  
+end 
+
+def sum_array(numbers)
+  #inject
+end 
+
+def add_s(string)
+  #.each_with_index + .collect
+  # [1,2].each_with_index.collect{|element, index| }
+end 
+  
+
+
 
